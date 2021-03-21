6ddae3d2aee45b1555802facc9ee25f087f231ab
unknown <>
2021-03-21 14:08:16 +0200

    test

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import preloader from "../../../assets/images/preloader.svg";
 
 let Preloader = (props) => {
-    return <div style={{ display: "flex"}}>
+    return <div style={{ display: "flex", height: '50vh'}}>
         <img style={{margin: "auto"}} src={preloader}/>
     </div>
 }
